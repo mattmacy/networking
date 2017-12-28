56a38082838a45ca10de1c08c134166cb94896f7
Matt Macy <mmacy@mattmacy.io>
2017-12-27 21:49:01 -0800

    add fast vxlan decap to vtnet

--- sys/dev/virtio/network/if_vtnetvar.h
+++ sys/dev/virtio/network/if_vtnetvar.h
@@ -144,6 +144,7 @@ struct vtnet_softc {
 #define VTNET_FLAG_MULTIQ	 0x0200
 #define VTNET_FLAG_INDIRECT	 0x0400
 #define VTNET_FLAG_EVENT_IDX	 0x0800
+	uint16_t	vtnet_vxlan_port;
 
 	int			 vtnet_link_active;
 	int			 vtnet_hdr_size;
