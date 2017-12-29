886dbc666a5160dc9fb601c876859b5eaf802929
Matt Macy <mmacy@mattmacy.io>
2017-12-28 20:56:39 -0800

    fix magic name, remove redundant port decl

--- sys/net/if_vpc.h
+++ sys/net/if_vpc.h
@@ -1,7 +1,7 @@
 #ifndef __IF_VPC_H_
 #define __IF_VPC_H_
 
-#define VB_MAGIC 0x20171202
+#define VPC_VERS 0x20171228
 struct vpc_ioctl_header {
 	uint64_t vih_magic;
 	uint64_t vih_type;
@@ -9,9 +9,7 @@ struct vpc_ioctl_header {
 struct vpc_listen {
 	struct vpc_ioctl_header vl_vih;
 	struct sockaddr vl_addr;
-	uint16_t vl_port;
 };
-
 
 struct vpc_fte {
 	uint32_t vf_vni;
@@ -41,7 +39,7 @@ struct vpc_fte_list {
 
 
 struct vpci_attach {
-	struct vpc_ioctl_header vl_vih;
+	struct vpc_ioctl_header va_vih;
 	char va_ifname[IFNAMSIZ];
 };
 struct vpci_vni {
