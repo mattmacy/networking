440aff351c3b8a9aba5fe26ff7d509d7c4c05ea4
Matt Macy <mmacy@mattmacy.io>
2017-12-30 21:11:45 -0800

    check mvec / mvec TSO support

---
+++ sys/kern/uipc_mvec.c
@@ -0,0 +1,557 @@
+
+/*
+ * Copyright (C) 2017 Matthew Macy <matt.macy@joyent.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/malloc.h>
+#include <sys/types.h>
+#include <sys/systm.h>
+#include <sys/mbuf.h>
+#include <sys/kernel.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+#include <sys/smp.h>
+#include <sys/sysctl.h>
+
+static MALLOC_DEFINE(M_MVEC, "mvec", "mbuf vector");
+
+static int type2len[] = {-1, MCLBYTES, -1, MJUMPAGESIZE, MJUM9BYTES, MJUM16BYTES, -1, MSIZE};
+
+static void
+mvec_clfree(struct mvec_ent *me, m_refcnt_t *refcntp, bool dupref)
+{
+	bool free = true;
+	struct mbuf *mref;
+	volatile uint32_t *refcnt;
+
+	mref = NULL;
+	if (dupref) {
+		if (me->me_ext_flags & EXT_FLAG_EMBREF) {
+			refcnt = &refcntp->ext_count;
+		} else {
+			refcnt = refcntp->ext_cnt;
+		}
+		free = (*refcnt == 1 || atomic_fetchadd_int(refcnt, -1) == 1);
+	}
+	if (!free)
+		return;
+	if (!(me->me_ext_flags & EXT_FLAG_NOFREE))
+		mref =  __containerof(refcnt, struct mbuf, m_ext.ext_count);
+
+	switch (me->me_ext_type) {
+		case EXT_CLUSTER:
+			uma_zfree(zone_clust, me->me_cl);
+			break;
+		case EXT_JUMBOP:
+			uma_zfree(zone_jumbop, me->me_cl);
+			break;
+		case EXT_JUMBO9:
+			uma_zfree(zone_jumbo9, me->me_cl);
+			break;
+		case EXT_JUMBO16:
+			uma_zfree(zone_jumbo16, me->me_cl);
+			break;
+		default:
+			panic("unsupported ext_type: %d\n", me->me_ext_type);
+	}
+	if (mref != NULL)
+		uma_zfree_arg(zone_mbuf, mref, (void *)MB_DTOR_SKIP);
+}
+
+static void
+mvec_ent_free(struct mvec_header *mh, int idx)
+{
+	struct mvec_ent *me = (struct mvec_ent *)(mh + 1);
+	m_refcnt_t *me_count = (m_refcnt_t *)(me + mh->mh_count);
+
+	me += idx;
+	me_count += idx;
+	switch (me->me_type) {
+		case MVEC_MBUF:
+			uma_zfree_arg(zone_mbuf, me->me_cl, (void *)MB_DTOR_SKIP);
+			break;
+		case MVEC_MANAGED:
+			mvec_clfree(me, me_count, mh->mh_multiref);
+			break;
+		default:
+			/* ... */
+			break;
+	}
+}
+
+void
+mvec_offset(struct mbuf *m, struct mvec_cursor *mc, int offset)
+{
+	struct mvec_header *mh = MBUF2MH(m);
+	struct mvec_ent *me = MHME(mh);
+	int rem;
+
+	mc->mc_idx = mc->mc_off = 0;
+	MPASS(offset <= m->m_pkthdr.len);
+	rem = offset;
+
+	do {
+		if (rem > me->me_len) {
+			rem -= me->me_len;
+			me++;
+			mc->mc_idx++;
+		} else if (rem < me->me_len) {
+			rem = 0;
+			mc->mc_off = rem;
+		} else {
+			rem = 0;
+			mc->mc_idx++;
+		}
+	} while(rem);
+}
+
+void
+mvec_trim(struct mbuf *m, int offset)
+{
+	struct mvec_header *mh = MBUF2MH(m);
+	struct mvec_ent *me = MHME(mh);
+	int rem;
+
+	MPASS(offset <= m->m_pkthdr.len);
+	rem = offset;
+	do {
+		if (rem > me->me_len) {
+			rem -= me->me_len;
+			mvec_ent_free(mh, mh->mh_start);
+			mh->mh_start++;
+			me++;
+		} else if (rem < me->me_len) {
+			rem = 0;
+			me->me_off += rem;
+			me->me_len -= rem;
+		} else {
+			rem = 0;
+			mvec_ent_free(mh, mh->mh_start);
+			mh->mh_start++;
+		}
+	} while(rem);
+	m->m_data = ME_SEG(mh, 0);
+}
+
+struct mbuf *
+mvec_pullup(struct mbuf *m, int count)
+{
+	struct mvec_header *mh;
+	struct mvec_ent *mecur, *menxt;
+	int tailroom, type, size, copylen, doff, i, len;
+	
+	MPASS(size <= m->m_pkthdr.len);
+	mh = MBUF2MH(m);
+	mecur = MHMEI(mh, 0);
+	type = mecur->me_ext_type;
+	MPASS(type <= EXT_MBUF);
+	size = type2len[type];
+	MPASS(size > 0);
+	tailroom = size - mecur->me_off - mecur->me_len;
+	MPASS(headroom >= 0);
+	copylen = count - mecur->me_len;
+
+	if (copylen > size) {
+		/* allocate new buffer */
+	} else if (copylen > tailroom) {
+		/*
+		 * move data up if possible
+		 * else allocate new buffer
+		 */
+	}
+	doff = mecur->me_off + mecur->me_len;
+	i = 1;
+	do {
+		menxt = MHMEI(mh, i);
+		len = min(copylen, menxt->me_len);
+		bcopy(ME_SEG(mh, i), mecur->me_cl + doff, len);
+		doff += len;
+		mecur->me_len += len;
+		menxt->me_off += len;
+		menxt->me_len -= len;
+		copylen -= len;
+		i++;
+	} while (copylen);
+	i = 1;
+	while (MHMEI(mh, i)->me_len == 0)
+		i++;
+	if (__predict_false(i != 1)) {
+		mh->mh_start += (i - 1);
+		bcopy(mecur, MHMEI(mh, 0), sizeof(*mecur));
+	}
+	m->m_data = ME_SEG(mh, 0);
+	return (m);
+}
+
+void
+mvec_free(struct mbuf *m)
+{
+	struct mvec_header *mh;
+	struct mvec_ent *me;
+	m_refcnt_t *me_count;
+	int i;
+
+	mh = (struct mvec_header *)m->m_pktdat + sizeof(struct m_ext);
+	me = (struct mvec_ent *)(mh + 1);
+	me_count = (m_refcnt_t *)(me + mh->mh_count);
+		
+	for (i = 0; i < mh->mh_count; i++, me_count++, me++) {
+		switch (me->me_type) {
+			case MVEC_MBUF:
+				uma_zfree_arg(zone_mbuf, me->me_cl, (void *)MB_DTOR_SKIP);
+				break;
+			case MVEC_MANAGED:
+				mvec_clfree(me, me_count, mh->mh_multiref);
+				break;
+			default:
+				/* ... */
+				break;
+		}
+	}
+}
+
+
+static void
+mvec_header_init(struct mbuf *mnew)
+{
+	mnew->m_next = NULL;
+	mnew->m_nextpkt = NULL;
+	mnew->m_flags |= M_PKTHDR|M_NOFREE|M_EXT;
+	mnew->m_ext.ext_buf = (caddr_t)mnew;
+	mnew->m_ext.ext_flags = EXT_FLAG_EMBREF|EXT_FLAG_NOFREE;
+	mnew->m_ext.ext_count = 1;
+	mnew->m_ext.ext_type = EXT_MVEC;
+}
+
+struct mbuf *
+mchain_to_mvec(struct mbuf *m)
+{
+	struct mbuf *mp, *mnext, *mnew;
+	struct mvec_header *mh;
+	struct mvec_ent *me;
+	int count, size;
+	bool dupref;
+	m_refcnt_t *me_count, countp;
+
+	count = 0;
+	mp = m;
+	dupref = false;
+	do {
+		mnext = mp->m_next;
+		count++;
+		if (mp->m_flags & M_EXT) {
+			/*
+			 * bail on ext_free -- we can't efficiently pass an mbuf
+			 * at free time and m_ext adds up to a lot of space
+			 */
+			if (mp->m_ext.ext_free != NULL)
+				return (NULL);
+			if (!(mp->m_ext.ext_flags & EXT_FLAG_EMBREF && mp->m_ext.ext_count == 1))
+				dupref = true;
+		}
+		mp = mnext;
+	} while (mp);
+
+	size = count*sizeof(struct mvec_ent) + sizeof(*mh) + sizeof(struct mbuf);
+	if (dupref)
+		size += count*sizeof(void*);
+	mnew = malloc(size, M_MVEC, M_NOWAIT);
+	if (mnew == NULL)
+		return (NULL);
+	me_count = NULL;
+
+	mvec_header_init(mnew);
+	mnew->m_len = m->m_len;
+	mh = (struct mvec_header *)mnew->m_pktdat + sizeof(struct m_ext);
+	mh->mh_count = count;
+	mh->mh_multiref = dupref;
+	bcopy(&m->m_pkthdr, &mnew->m_pkthdr, sizeof(struct pkthdr));
+
+	me = (struct mvec_ent *)(mh + 1);
+	me_count = (m_refcnt_t *)(me + count);
+	do {
+		mnext = mp->m_next;
+		countp.ext_cnt = NULL;
+		if (mp->m_flags & M_EXT) {
+			bool canfree = true;
+
+			me->me_cl = mp->m_ext.ext_buf;
+			me->me_off = ((uintptr_t)mp->m_data - (uintptr_t)mp->m_ext.ext_buf);
+			me->me_len = mp->m_len;
+			me->me_eop = 0;
+			me->me_type = MVEC_MANAGED;
+			me->me_ext_flags = mp->m_ext.ext_flags;
+			me->me_ext_type = mp->m_ext.ext_type;
+			if (!(mp->m_ext.ext_flags & EXT_FLAG_EMBREF)) {
+				countp.ext_cnt = mp->m_ext.ext_cnt;	
+			} else if (mp->m_ext.ext_flags & EXT_FLAG_EMBREF) {
+				if (mp->m_ext.ext_count > 1) {
+					countp.ext_cnt = &mp->m_ext.ext_count;
+					canfree = false;
+					me->me_ext_flags &= ~EXT_FLAG_EMBREF;
+				} else
+					countp.ext_count = 0x1;
+			}
+			if (mp->m_flags & M_NOFREE) {
+				canfree = false;
+				me->me_ext_flags |= EXT_FLAG_NOFREE;
+			}
+			if (canfree)
+				m_free(mp);
+			mp = NULL;
+		} else {
+			me->me_cl = (caddr_t)mp;
+			me->me_off = ((uintptr_t)(mp->m_data) - (uintptr_t)mp);
+			me->me_len = mp->m_len;
+			me->me_eop = 0;
+			me->me_type = MVEC_MBUF;
+			me->me_ext_flags = 0;
+			me->me_ext_type = EXT_MBUF;
+			countp.ext_cnt = 0;
+		}
+		if (dupref) {
+			*me_count = countp;
+			me_count++;
+		}
+		mp = mnext;
+		me++;
+	} while (mp);
+	return (mnew);
+}	
+
+/*
+ * Move the below to net/ once working 
+ */
+
+#include <sys/socket.h>
+#include <net/if.h>
+#include <net/if_var.h>
+#include <net/ethernet.h>
+#include <net/iflib.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+
+#include <machine/in_cksum.h>
+
+#define MIN_HDR_LEN   (ETHER_HDR_LEN + sizeof(struct ip) + sizeof(struct tcphdr))
+
+static int
+mvec_parse_header(struct mbuf *m, int prehdrlen, if_pkt_info_t pi)
+{
+	struct ether_vlan_header *evh;
+	struct mvec_header *mh = MBUF2MH(m);
+	struct mvec_ent *me = MHMEI(mh, 0);
+		
+	if (__predict_false(me->me_len - prehdrlen < MIN_HDR_LEN) &&
+		__predict_false(mvec_pullup(m, prehdrlen + MIN_HDR_LEN) == NULL))
+			return (ENOMEM);
+	evh = (struct ether_vlan_header *)(ME_SEG(mh, 0) + prehdrlen);
+	if (evh->evl_encap_proto == htons(ETHERTYPE_VLAN)) {
+		pi->ipi_etype = ntohs(evh->evl_proto);
+		pi->ipi_ehdrlen = ETHER_HDR_LEN + ETHER_VLAN_ENCAP_LEN;
+	} else {
+		pi->ipi_etype = ntohs(evh->evl_encap_proto);
+		pi->ipi_ehdrlen = ETHER_HDR_LEN;
+	}
+	switch (pi->ipi_etype) {
+		case ETHERTYPE_IP: {
+			struct ip *ip = NULL;
+			struct tcphdr *th = NULL;
+			int minthlen;
+
+			minthlen = pi->ipi_ehdrlen + sizeof(*ip) + sizeof(*th);
+			if (__predict_false(me->me_len - prehdrlen < minthlen) &&
+				__predict_false(mvec_pullup(m, prehdrlen + minthlen) == NULL))
+				return (ENOMEM);
+			me = MHMEI(mh, 0);
+			ip = (struct ip *)(ME_SEG(mh, 0) + prehdrlen + pi->ipi_ehdrlen);
+			pi->ipi_ip_hlen = ip->ip_hl << 2;
+			pi->ipi_ipproto = ip->ip_p;
+			if (ip->ip_p != IPPROTO_TCP)
+				return (EINVAL);
+			minthlen = pi->ipi_ehdrlen + pi->ipi_ip_hlen + sizeof(*th);
+			if (__predict_false(me[0].me_len - prehdrlen < minthlen) &&
+				__predict_false(mvec_pullup(m, prehdrlen + minthlen) == NULL))
+				return (ENOMEM);
+			me = MHMEI(mh, 0);
+			th = (struct tcphdr *)(ME_SEG(mh, 0) + prehdrlen + pi->ipi_ehdrlen + pi->ipi_ip_hlen);
+			pi->ipi_tcp_hflags = th->th_flags;
+			pi->ipi_tcp_hlen = th->th_off << 2;
+			pi->ipi_tcp_seq = th->th_seq;
+			minthlen = pi->ipi_ehdrlen + pi->ipi_ip_hlen + pi->ipi_tcp_hlen;
+			if (__predict_false(me[0].me_len - prehdrlen < minthlen) &&
+				__predict_false(mvec_pullup(m, prehdrlen + minthlen) == NULL))
+				return (ENOMEM);
+			me = MHMEI(mh, 0);
+			if (prehdrlen == 0) {
+				th->th_sum = in_pseudo(ip->ip_src.s_addr,
+									   ip->ip_dst.s_addr, htons(IPPROTO_TCP));
+				ip->ip_sum = 0;
+				ip->ip_len = htons(pi->ipi_ip_hlen + pi->ipi_tcp_hlen + pi->ipi_tso_segsz);
+
+			}
+			
+			break;
+		}
+		case ETHERTYPE_IPV6: {
+			break;
+		}
+		default:
+			/* XXX unsupported -- error */
+			break;
+	}
+	return (0);
+}
+
+struct mbuf *
+mvec_tso(struct mbuf *m, int prehdrlen)
+{
+	struct mvec_header *mh, *newmh;
+	struct mvec_ent *me, *mesrc, *medst, *newme;
+	struct mbuf *mnew;
+	struct if_pkt_info pi;
+	m_refcnt_t *newme_count, *medst_count, *mesrc_count;
+	int segcount, soff, segrem, srem;
+	int i, ntsofrags, segsz, cursegrem, nheaders, hdrsize, rem, curseg, count, size;
+	struct tcphdr *th;
+	caddr_t hdrbuf;
+
+	segsz = m->m_pkthdr.tso_segsz;
+	mh = (struct mvec_header *)(m->m_pktdat + sizeof(struct m_ext));
+	me = (struct mvec_ent *)(mh + 1);
+	pi.ipi_tso_segsz = segsz;
+	if (mvec_parse_header(m, prehdrlen, &pi))
+		return (NULL);
+	hdrsize = prehdrlen + pi.ipi_ehdrlen + pi.ipi_ip_hlen + pi.ipi_tcp_hlen;
+	
+	nheaders = m->m_pkthdr.len / segsz;
+	if (nheaders*segsz != m->m_pkthdr.len)
+		nheaders++;
+	for (segcount = i = 0; i < mh->mh_count; i++, me++) {
+		rem = me->me_len;
+		if (rem < cursegrem) {
+			cursegrem -= rem;
+			segcount++;
+		} else if (rem == cursegrem) {
+			segcount++;
+			ntsofrags = 0;
+			cursegrem = segsz;
+		} else {
+			while (rem) {
+				curseg = min(rem, cursegrem);
+				rem -= curseg;
+				cursegrem -= curseg;
+				if (!cursegrem)
+					cursegrem = segsz;
+				segcount++;
+			}
+		}
+	}
+
+	count = segcount + nheaders;
+	size = count*sizeof(struct mvec_ent) + sizeof(*mh) + sizeof(struct mbuf);
+	if (mh->mh_multiref)
+		size += count*sizeof(void*);
+	size += nheaders * hdrsize;
+	mnew = malloc(size, M_MVEC, M_NOWAIT);	
+	mvec_header_init(mnew);
+	bcopy(&m->m_pkthdr, &mnew->m_pkthdr, sizeof(struct pkthdr));
+	mnew->m_len = m->m_len;
+	newmh = (struct mvec_header *)mnew->m_pktdat + sizeof(struct m_ext);
+	newmh->mh_count = count;
+	newmh->mh_multiref = mh->mh_multiref;
+	newme = (struct mvec_ent *)(mh + 1);
+	newme_count = (m_refcnt_t *)(me + count);
+
+	medst_count = newme_count;
+	medst = newme;
+	mesrc_count = (m_refcnt_t *)(me + mh->mh_count);
+	mesrc = me;
+	
+	soff = 0;
+	MPASS(mesrc->me_len >= hdrsize);
+	if (mesrc->me_len == hdrsize)
+		mesrc++;
+	else if (mesrc->me_len > hdrsize)
+		soff = hdrsize;
+	hdrbuf = (caddr_t)(newme + count);
+	bzero(medst_count, count*sizeof(void *));
+	for (i = 0; i < nheaders; i++) {
+		bcopy(ME_SEG(mh, 0), hdrbuf, hdrsize);
+		th = (struct tcphdr *)(hdrbuf + (hdrsize - pi.ipi_tcp_hlen));
+		/* fixup tcphdr XXX */
+		medst->me_cl = hdrbuf;
+		medst->me_off = 0;
+		medst->me_len = hdrsize;
+		medst->me_type = MVEC_UNMANAGED;
+		medst->me_ext_flags = 0;
+		medst->me_ext_type = 0;
+		medst->me_eop = 0;
+		segrem = segsz;
+		soff = 0;
+		medst_count++;
+		do {
+			if (soff == 0) {
+				*medst_count = *mesrc_count;
+				mesrc_count++;
+				medst->me_type = mesrc->me_type;
+				medst->me_ext_flags = mesrc->me_ext_flags;
+				medst->me_ext_type = mesrc->me_ext_type;
+			} else {
+				medst->me_type = MVEC_UNMANAGED;
+				medst->me_ext_flags = 0;
+				medst->me_ext_type = 0;
+			}
+			srem = mesrc->me_len - soff;
+			medst->me_cl = mesrc->me_cl;
+			medst->me_off = mesrc->me_off + soff;
+			if (srem == segrem) {
+				medst->me_eop = 1;
+				soff = 0;
+				medst->me_len = srem;
+				mesrc++;
+			} else if (srem < segrem) {
+				medst->me_eop = 0;
+				soff = 0;
+				medst->me_len = srem;
+				segrem -= srem;
+				mesrc++;
+			} else {
+				medst->me_eop = 1;
+				soff += segrem;
+				medst->me_len = segrem;
+			}				
+			medst++;
+			medst_count++;
+		} while (medst->me_eop == 0);
+		hdrbuf += hdrsize;		
+	}
+	return (0);
+}
