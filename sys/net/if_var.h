1361899c96cc00d4c1d766831cee70d3c105fd18
Matt Macy <mmacy@mattmacy.io>
2018-01-01 16:22:09 -0800

    pay token homage to setfn accessors

--- sys/net/if_var.h
+++ sys/net/if_var.h
@@ -709,6 +709,8 @@ void if_setinitfn(if_t ifp, void (*)(void *));
 void if_setioctlfn(if_t ifp, int (*)(if_t, u_long, caddr_t));
 void if_setstartfn(if_t ifp, void (*)(if_t));
 void if_settransmitfn(if_t ifp, if_transmit_fn_t);
+void if_settransmittxqfn(if_t ifp, if_transmit_fn_t);
+void if_setmbuftoqidfn(if_t ifp, if_transmit_fn_t);
 void if_setqflushfn(if_t ifp, if_qflush_fn_t);
 void if_setgetcounterfn(if_t ifp, if_get_counter_t);
  
