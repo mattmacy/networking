3bbb783ba4b3b1cddd643494a281154f8d1ca35e
Matt Macy <mmacy@mattmacy.io>
2018-01-10 19:27:31 -0800

    add mvec support to sglist

--- sys/sys/sglist.h
+++ sys/sys/sglist.h
@@ -88,6 +88,7 @@ struct sglist *sglist_alloc(int nsegs, int mflags);
 int	sglist_append(struct sglist *sg, void *buf, size_t len);
 int	sglist_append_bio(struct sglist *sg, struct bio *bp);
 int	sglist_append_mbuf(struct sglist *sg, struct mbuf *m0);
+int	sglist_append_mvec(struct sglist *sg, struct mbuf *m0);
 int	sglist_append_phys(struct sglist *sg, vm_paddr_t paddr,
 	    size_t len);
 int	sglist_append_sglist(struct sglist *sg, struct sglist *source,
