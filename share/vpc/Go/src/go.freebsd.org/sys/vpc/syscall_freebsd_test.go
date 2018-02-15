9d4925b6a05754b9a6e440a5e85437873d6b0f34
Sean Chittenden <sean@chittenden.org>
2018-02-15 11:38:06 -0800

    Commit miss: add sanity test for vpc_open(2).

---
+++ share/vpc/Go/src/go.freebsd.org/sys/vpc/syscall_freebsd_test.go
@@ -0,0 +1,60 @@
+// Go interface to OS-independent VPC syscalls.
+//
+// SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+//
+// Copyright (c) 2018 Joyent, Inc.
+// All rights reserved.
+//
+// This software was developed by Sean Chittenden <seanc@FreeBSD.org> at Joyent,
+// Inc.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+package vpc_test
+
+import (
+	"testing"
+
+	"go.freebsd.org/sys/vpc"
+)
+
+func TestVPCOpenClose(t *testing.T) {
+	id := vpc.GenID()
+
+	ht, err := vpc.NewHandleType(vpc.HandleTypeInput{
+		Version: 1,
+		Type:    vpc.ObjTypeSwitch,
+	})
+	if err != nil {
+		t.Fatalf("unable to construct a HandleType: %v", err)
+	}
+
+	h, err := vpc.Open(id, ht, vpc.FlagCreate)
+	if err != nil {
+		t.Fatalf("vpc_open(2) failed: %v", err)
+	}
+
+	err = h.Close()
+	if err != nil {
+		t.Fatalf("unable to close(2) VPC Handle : %v", err)
+	}
+}
