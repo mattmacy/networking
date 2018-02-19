7473b4e789697aea7e6a94461620e71b52525502
Sean Chittenden <sean@chittenden.org>
2018-02-18 21:06:51 -0800

    WIP commit
    This module has advanced and is no longer using the old `OpFlag`
    type, but isn't complete.  Commit so that tests can be shared/reused.

--- libexec/go.freebsd.org/sys/vpc/vpcsw/ops.go
+++ libexec/go.freebsd.org/sys/vpc/vpcsw/ops.go
@@ -29,13 +29,83 @@
 
 package vpcsw
 
-import "go.freebsd.org/sys/vpc"
+import (
+	"github.com/pkg/errors"
+	"go.freebsd.org/sys/vpc"
+)
 
-type _SwitchOp vpc.OpFlag
+/*
+ * Cmd
+ *   Direction
+ *   ObjType
+ *   Op
+ *
+ * InArg
+ *   []byte
+ * OutArg
+ *   *[]byte
+ */
 
-var _CreateOp _SwitchOp
+// _SwitchCmd is the encoded type of operations that can be performed on a VPC
+// Switch.
+type _SwitchCmd vpc.Cmd
+
+// _SwitchCmdSetArgType is the value used by a VPC Switch Set operation.
+type _SwitchSetOpArgType uint64
+
+const (
+	// Bits for input
+	_DownBit _SwitchSetOpArgType = 0x00000000
+	_UpBit   _SwitchSetOpArgType = 0x00000001
+)
+
+// Ops that can be encoded into a vpc.Cmd
+const (
+	_OpInvalid       = vpc.Op(0)
+	_OpPortAdd       = vpc.Op(1)
+	_OpPortDel       = vpc.Op(2)
+	_OpPortUplinkSet = vpc.Op(3)
+	_OpPortUplinkGet = vpc.Op(4)
+	_OpStateGet      = vpc.Op(5)
+	_OpStateSet      = vpc.Op(6)
+	_OpReset         = vpc.Op(7)
+)
+
+// Template commands that can be passed to vpc.Ctl() with a valid VPC Switch
+// Handle.
+var (
+	_PortAddCmd    _SwitchCmd
+	_PortDelCmd    _SwitchCmd
+	_PortUplinkSet _SwitchCmd
+	_PortUplinkGet _SwitchCmd
+	_PortStateSet  _SwitchCmd
+	_PortStateGet  _SwitchCmd
+	_ResetCmd      _SwitchCmd
+)
 
 func init() {
-	createOp := vpc.MutateBit | vpc.InBit
-	_CreateOp = _SwitchOp(createOp)
+	// {
+	// 	createOp := vpc.MutateBit | vpc.InBit
+	// 	_CreateOp = _SwitchOp(createOp)
+	// }
+
+	// {
+	// 	resetCmd := vpc.MutateBit | _ResetCmd
+	// 	_ResetCmd = _SwitchCmd(resetCmd)
+	// }
+}
+
+// Reset resets the VPC Switch.
+func (sw *VPCSW) Reset() error {
+	if sw.h <= 0 {
+		return nil
+	}
+
+	// TODO(seanc@): Test to see make sure the descriptor has the mutate bit set.
+
+	if err := vpc.Ctl(sw.h, vpc.Cmd(_ResetCmd), nil, nil); err != nil {
+		return errors.Wrap(err, "unable to reset VPC Switch")
+	}
+
+	return nil
 }
