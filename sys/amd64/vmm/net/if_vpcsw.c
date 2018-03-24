98e0af9d3e6f1e2a6c4e9d58a79ec4ac957bf194
Matt Macy <mmacy@mattmacy.io>
2018-03-24 11:16:15 -0700

    we don't need to return on mbuf in transit, broadcast will handle localhost as well

--- sys/amd64/vmm/net/if_vpcsw.c
+++ sys/amd64/vmm/net/if_vpcsw.c
@@ -466,7 +466,7 @@ vpcsw_process_one(struct vpcsw_softc *vs, struct vpcsw_cache_ent *cache, struct 
 }
 
 static int
-vpcsw_transit(struct vpcsw_softc *vs, struct vpcsw_cache_ent *cache, struct mbuf *m, struct mbuf **mret)
+vpcsw_transit(struct vpcsw_softc *vs, struct vpcsw_cache_ent *cache, struct mbuf *m)
 {
 	struct ifnet *ifnext;
 	struct mbuf *mh, *mt, *mnext;
@@ -487,10 +487,7 @@ vpcsw_transit(struct vpcsw_softc *vs, struct vpcsw_cache_ent *cache, struct mbuf
 		if (__predict_false(rc))
 			break;
 		if (__predict_false(m->m_pkthdr.rcvif == NULL)) {
-			if (mret && *mret == NULL)
-				*mret = m;
-			else
-				m_freem(m);
+			m_freem(m);
 			goto next;
 		}
 		if (mh == NULL) {
@@ -547,7 +544,7 @@ vpcsw_transmit_ext(struct ifnet *ifp, struct mbuf *m, void *cache)
 	struct vpcsw_softc *vs;
 
 	vs = iflib_get_softc(ctx);
-	return (vpcsw_transit(vs, cache, m, NULL));
+	return (vpcsw_transit(vs, cache, m));
 }
 
 static int
@@ -562,7 +559,7 @@ vpcsw_bridge_output(struct ifnet *ifp, struct mbuf *m, struct sockaddr *s __unus
 	ctx = ifp->if_softc;
 	cache = vpcp_get_pcpu_cache(ctx);
 	MPASS(cache != NULL);
-	return (vpcsw_transit(vs, cache, m, NULL));
+	return (vpcsw_transit(vs, cache, m));
 }
 
 static void
@@ -585,8 +582,8 @@ vpcsw_bridge_input(if_t ifp, struct mbuf *m)
 	cache = vpcp_get_pcpu_cache(ctx);
 	MPASS(cache != NULL);
 
-	vpcsw_transit(vs, cache, m, &mp);
-	return (mp);
+	vpcsw_transit(vs, cache, m);
+	return (NULL);
 }
 
 static int
@@ -807,7 +804,7 @@ vpcsw_ndv4_resp_send(struct vpcsw_softc *vs, const struct vpcsw_response *rsp)
 		m->m_pkthdr.ether_vtag = rsp->vrs_context.voc_vtag;
 		m->m_flags |= M_VLANTAG;
 	}
-	return (vpcsw_transit(vs, NULL, m, NULL));
+	return (vpcsw_transit(vs, NULL, m));
 }
 
 static int
