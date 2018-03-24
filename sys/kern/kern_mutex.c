6853a022fe416b774834c4ed01b0b299e907be34
Matt Macy <mmacy@mattmacy.io>
2018-03-23 23:53:25 -0700

    give more info when lock held too long

--- sys/kern/kern_mutex.c
+++ sys/kern/kern_mutex.c
@@ -1214,8 +1214,9 @@ _mtx_lock_indefinite_check(struct mtx *m, struct lock_delay_arg *ldap)
 		if (td == NULL)
 			return;
 
-		printf( "spin lock %p (%s) held by %p (tid %d) too long\n",
-		    m, m->lock_object.lo_name, td, td->td_tid);
+		printf("spin lock %p (%s) held by %p (tid %d %s %s) too long\n",
+			   m, m->lock_object.lo_name, td, td->td_tid,
+			   td->td_name, td->td_proc->p_comm);
 #ifdef WITNESS
 		witness_display_spinlock(&m->lock_object, td, printf);
 #endif
