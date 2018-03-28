b426bee6f4eac48b171b8b4b9e348302e7b8f3dc
Matt Macy <mmacy@mattmacy.io>
2018-03-27 23:50:44 -0700

    remove overflow check

--- sys/amd64/vmm/net/if_ethlink.c
+++ sys/amd64/vmm/net/if_ethlink.c
@@ -347,8 +347,6 @@ ethlink_ctl(vpc_ctx_t ctx, vpc_op_t op, size_t inlen, const void *in,
 			if (inlen != sizeof(uint16_t))
 				return (EBADRPC);
 			vtag = *(const uint16_t *)in;
-			if (ntohs(vtag) > ((1<<12)-1))
-				return (EOVERFLOW);
 			es->es_vtag = vtag;
 			break;
 		}
