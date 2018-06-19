6f0e422d452de369b41ba726a2656a8630894595
Matt Macy <mmacy@mattmacy.io>
2018-06-19 01:08:45 -0700

    move swap_reserve to atomic_fetchadd and use pcpu caches where possible

--- sys/vm/vm.h
+++ sys/vm/vm.h
@@ -151,11 +151,11 @@ extern int old_mlock;
 extern int vm_ndomains;
 
 struct ucred;
-int swap_reserve(vm_ooffset_t incr);
-int swap_reserve_by_cred(vm_ooffset_t incr, struct ucred *cred);
-void swap_reserve_force(vm_ooffset_t incr);
-void swap_release(vm_ooffset_t decr);
-void swap_release_by_cred(vm_ooffset_t decr, struct ucred *cred);
+int swap_reserve(vm_offset_t incr);
+int swap_reserve_by_cred(vm_offset_t incr, struct ucred *cred);
+void swap_reserve_force(vm_offset_t incr);
+void swap_release(vm_offset_t decr);
+void swap_release_by_cred(vm_offset_t decr, struct ucred *cred);
 void swapper(void);
 
 #endif				/* VM_H */
