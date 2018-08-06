bb842dedc9197b3bdb04fd9dfdad49ab1fa262f0
Sean Eric Fagan <sef@ixsystems.com>
2018-08-06 13:17:02 -0700

    An attempt to merge in the crypto changes with a newly-merged freebsd

--- sys/opencrypto/cryptodev.h
+++ sys/opencrypto/cryptodev.h
@@ -85,6 +85,8 @@
 #define	MD5_KPDK_HASH_LEN	16
 #define	SHA1_KPDK_HASH_LEN	20
 #define	AES_GMAC_HASH_LEN	16
+#define	AES_CBC_MAC_HASH_LEN	16
+
 /* Maximum hash algorithm result length */
 #define	HASH_MAX_LEN		SHA2_512_HASH_LEN /* Keep this updated */
 
@@ -106,6 +108,9 @@
 #define	AES_128_GMAC_KEY_LEN		16
 #define	AES_192_GMAC_KEY_LEN		24
 #define	AES_256_GMAC_KEY_LEN		32
+#define	AES_128_CBC_MAC_KEY_LEN		16
+#define	AES_192_CBC_MAC_KEY_LEN		24
+#define	AES_256_CBC_MAC_KEY_LEN		32
 
 /* Encryption algorithm block sizes */
 #define	NULL_BLOCK_LEN		4	/* IPsec to maintain alignment */
@@ -126,6 +131,7 @@
 
 #define	ARC4_IV_LEN		1
 #define	AES_GCM_IV_LEN		12
+#define	AES_CCM_IV_LEN		12
 #define	AES_XTS_IV_LEN		8
 #define	AES_XTS_ALPHA		0x87	/* GF(2^128) generator polynomial */
 
@@ -195,7 +201,11 @@
 #define	CRYPTO_SHA2_256		35
 #define	CRYPTO_SHA2_384		36
 #define	CRYPTO_SHA2_512		37
-#define	CRYPTO_ALGORITHM_MAX	37 /* Keep updated - see below */
+#define	CRYPTO_AES_CCM_16	38	/* cipher side */
+#define	CRYPTO_AES_128_CCM_CBC_MAC	39	/* auth side */
+#define	CRYPTO_AES_192_CCM_CBC_MAC	40	/* auth side */
+#define	CRYPTO_AES_256_CCM_CBC_MAC	41	/* auth side */
+#define	CRYPTO_ALGORITHM_MAX	32 /* Keep updated - see below */
 
 #define	CRYPTO_ALGO_VALID(x)	((x) >= CRYPTO_ALGORITHM_MIN && \
 				 (x) <= CRYPTO_ALGORITHM_MAX)
