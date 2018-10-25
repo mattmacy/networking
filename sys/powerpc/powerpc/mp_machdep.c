60b7123e759e2b6c56cd1cf3cd4cbcf051b1880d
Matt Macy <mmacy@FreeBSD.org>
2018-10-25 05:02:56 -0700

    use minimal priority on spinning AP on ppc64

--- sys/powerpc/powerpc/mp_machdep.c
+++ sys/powerpc/powerpc/mp_machdep.c
@@ -77,9 +77,14 @@ machdep_ap_bootstrap(void)
 	PCPU_SET(awake, 1);
 	__asm __volatile("msync; isync");
 
+#ifdef __powerpc64__ 
+	while (ap_letgo == 0)
+		__asm __volatile("or 31, 31, 31");
+#else
 	while (ap_letgo == 0)
 		__asm __volatile("or 27,27,27");
-	__asm __volatile("or 6,6,6");
+#endif
+	__asm __volatile("or 2, 2, 2");
 
 	/*
 	 * Set timebase as soon as possible to meet an implicit rendezvous
