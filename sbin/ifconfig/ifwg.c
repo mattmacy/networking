0406525336c9e6d4251856e981750eea2608a299
Matt Macy <mmacy@FreeBSD.org>
2020-02-19 13:44:26 -0800

    Parse keys and pass in with create request

--- sbin/ifconfig/ifwg.c
+++ sbin/ifconfig/ifwg.c
@@ -61,10 +61,108 @@
 #define WGC_GETCONF	0x2
 
 static nvlist_t *nvl_params;
-struct nvlist_desc {
-	caddr_t nd_data;
-	u_long nd_len;
+
+#define WG_KEY_LEN 32
+#define WG_KEY_LEN_BASE64 ((((WG_KEY_LEN) + 2) / 3) * 4 + 1)
+#define WG_KEY_LEN_HEX (WG_KEY_LEN * 2 + 1)
+
+static void encode_base64(u_int8_t *, u_int8_t *, u_int16_t);
+static bool decode_base64(u_int8_t *, u_int16_t, const u_int8_t *);
+
+const static u_int8_t Base64Code[] =
+"./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+const static u_int8_t index_64[128] = {
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 0, 1, 54, 55,
+        56, 57, 58, 59, 60, 61, 62, 63, 255, 255,
+        255, 255, 255, 255, 255, 2, 3, 4, 5, 6,
+        7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+        255, 255, 255, 255, 255, 255, 28, 29, 30,
+        31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+        51, 52, 53, 255, 255, 255, 255, 255
 };
+#define CHAR64(c)  ( (c) > 127 ? 255 : index_64[(c)])
+static bool
+decode_base64(u_int8_t *buffer, u_int16_t len, const u_int8_t *data)
+{
+        u_int8_t *bp = buffer;
+        const u_int8_t *p = data;
+        u_int8_t c1, c2, c3, c4;
+        while (bp < buffer + len) {
+                c1 = CHAR64(*p);
+                c2 = CHAR64(*(p + 1));
+
+                /* Invalid data */
+                if (c1 == 255 || c2 == 255)
+                        break;
+
+                *bp++ = (c1 << 2) | ((c2 & 0x30) >> 4);
+                if (bp >= buffer + len)
+                        break;
+
+                c3 = CHAR64(*(p + 2));
+                if (c3 == 255)
+                        break;
+
+                *bp++ = ((c2 & 0x0f) << 4) | ((c3 & 0x3c) >> 2);
+                if (bp >= buffer + len)
+                        break;
+
+                c4 = CHAR64(*(p + 3));
+                if (c4 == 255)
+                        break;
+                *bp++ = ((c3 & 0x03) << 6) | c4;
+
+                p += 4;
+        }
+		return (bp == buffer + len);
+}
+
+static void
+encode_base64(u_int8_t *buffer, u_int8_t *data, u_int16_t len)
+{
+        u_int8_t *bp = buffer;
+        u_int8_t *p = data;
+        u_int8_t c1, c2;
+        while (p < data + len) {
+                c1 = *p++;
+                *bp++ = Base64Code[(c1 >> 2)];
+                c1 = (c1 & 0x03) << 4;
+                if (p >= data + len) {
+                        *bp++ = Base64Code[c1];
+                        break;
+                }
+                c2 = *p++;
+                c1 |= (c2 >> 4) & 0x0f;
+                *bp++ = Base64Code[c1];
+                c1 = (c2 & 0x0f) << 2;
+                if (p >= data + len) {
+                        *bp++ = Base64Code[c1];
+                        break;
+                }
+                c2 = *p++;
+                c1 |= (c2 >> 6) & 0x03;
+                *bp++ = Base64Code[c1];
+                *bp++ = Base64Code[c2 & 0x3f];
+        }
+        *bp = '\0';
+}
+
+static bool
+key_from_base64(uint8_t key[static WG_KEY_LEN], const char *base64)
+{
+
+	if (strlen(base64) != WG_KEY_LEN_BASE64 - 1 || base64[WG_KEY_LEN_BASE64 - 2] != '=')
+		return false;
+
+	return (decode_base64(key, WG_KEY_LEN, base64));
+}
 
 static
 DECL_CMD_FUNC(setwglistenport, val, d)
@@ -77,6 +175,26 @@ DECL_CMD_FUNC(setwglistenport, val, d)
 		errx(1, "invalid value for listen-port");
 
 	nvlist_add_number(nvl_params, "listen-port", ul);
+}
+
+static
+DECL_CMD_FUNC(setwgprivkey, val, d)
+{
+	uint8_t key[WG_KEY_LEN];
+
+	if (!key_from_base64(key, val))
+		errx(1, "invalid key %s", val);
+	nvlist_add_binary(nvl_params, "private-key", key, WG_KEY_LEN);
+}
+
+static
+DECL_CMD_FUNC(setwgpubkey, val, d)
+{
+	uint8_t key[WG_KEY_LEN];
+
+	if (!key_from_base64(key, val))
+		errx(1, "invalid key %s", val);
+	nvlist_add_binary(nvl_params, "public-key", key, WG_KEY_LEN);
 }
 
 static int
@@ -148,7 +266,9 @@ wireguard_status(int s)
 }
 
 static struct cmd wireguard_cmds[] = {
-    DEF_CMD_ARG("listenport",  setwglistenport),
+    DEF_CLONE_CMD_ARG("listen-port",  setwglistenport),
+    DEF_CLONE_CMD_ARG("private-key",  setwgprivkey),
+    DEF_CLONE_CMD_ARG("public-key",  setwgpubkey),
 };
 
 static struct afswtch af_wireguard = {
@@ -160,19 +280,24 @@ static struct afswtch af_wireguard = {
 static void
 wg_create(int s, struct ifreq *ifr)
 {
-	struct nvlist_desc nd;
+	struct iovec iov;
 	void *packed;
 	size_t size;
 
+	setproctitle("ifconfig %s create ...\n", name);
 	if (!nvlist_exists_number(nvl_params, "listen-port"))
 		errx(1, "must specify a listen-port for wg create");
+	if (!nvlist_exists_binary(nvl_params, "public-key"))
+		errx(1, "must specify a public-key for wg create");
+	if (!nvlist_exists_binary(nvl_params, "private-key"))
+		errx(1, "must specify a private-key for wg create");
 
 	packed = nvlist_pack(nvl_params, &size);
 	if (packed == NULL)
 		errx(1, "failed to setup create request");
-	nd.nd_len = size;
-	nd.nd_data = packed;
-	ifr->ifr_data = (caddr_t)&nd;
+	iov.iov_len = size;
+	iov.iov_base = packed;
+	ifr->ifr_data = (caddr_t)&iov;
 	if (ioctl(s, SIOCIFCREATE2, ifr) < 0)
 		err(1, "SIOCIFCREATE2");
 }
