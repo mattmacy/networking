1f5f7137033179513340f9cd5c4424053fba2bf7
Matt Macy <mmacy@FreeBSD.org>
2020-02-20 12:27:32 -0800

    convert base64 handling to standard variant and improve key decode warnings

--- sbin/ifconfig/ifwg.c
+++ sbin/ifconfig/ifwg.c
@@ -70,97 +70,108 @@ static void encode_base64(u_int8_t *, u_int8_t *, u_int16_t);
 static bool decode_base64(u_int8_t *, u_int16_t, const u_int8_t *);
 
 const static u_int8_t Base64Code[] =
-"./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 const static u_int8_t index_64[128] = {
         255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
         255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
         255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
         255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-        255, 255, 255, 255, 255, 255, 0, 1, 54, 55,
-        56, 57, 58, 59, 60, 61, 62, 63, 255, 255,
-        255, 255, 255, 255, 255, 2, 3, 4, 5, 6,
-        7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
-        255, 255, 255, 255, 255, 255, 28, 29, 30,
-        31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-        41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-        51, 52, 53, 255, 255, 255, 255, 255
+        255, 255, 255, 62, 255, 255, 255, 63, 52, 53,
+        54, 55, 56, 57, 58, 59, 60, 61, 255, 255,
+        255, 255, 255, 255, 255, 0, 1, 2, 3, 4,
+        5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+        255, 255, 255, 255, 255, 255, 26, 27, 28,
+        29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+        39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+        49, 50, 51, 255, 255, 255, 255, 255
 };
 #define CHAR64(c)  ( (c) > 127 ? 255 : index_64[(c)])
 static bool
 decode_base64(u_int8_t *buffer, u_int16_t len, const u_int8_t *data)
 {
-        u_int8_t *bp = buffer;
-        const u_int8_t *p = data;
-        u_int8_t c1, c2, c3, c4;
-        while (bp < buffer + len) {
-                c1 = CHAR64(*p);
-                c2 = CHAR64(*(p + 1));
+	const uint8_t *p = data;
+	uint8_t *bp = buffer;
+	uint8_t c1, c2, c3, c4;
 
-                /* Invalid data */
-                if (c1 == 255 || c2 == 255)
-                        break;
+	while (bp < buffer + len) {
+		c1 = CHAR64(*p);
+		c2 = CHAR64(*(p + 1));
 
-                *bp++ = (c1 << 2) | ((c2 & 0x30) >> 4);
-                if (bp >= buffer + len)
-                        break;
+		/* Invalid data */
+		if (c1 == 255 || c2 == 255)
+			break;
 
-                c3 = CHAR64(*(p + 2));
-                if (c3 == 255)
-                        break;
+		*bp++ = (c1 << 2) | ((c2 & 0x30) >> 4);
+		if (bp >= buffer + len)
+			break;
 
-                *bp++ = ((c2 & 0x0f) << 4) | ((c3 & 0x3c) >> 2);
-                if (bp >= buffer + len)
-                        break;
+		c3 = CHAR64(*(p + 2));
+		if (c3 == 255)
+			break;
 
-                c4 = CHAR64(*(p + 3));
-                if (c4 == 255)
-                        break;
-                *bp++ = ((c3 & 0x03) << 6) | c4;
+		*bp++ = ((c2 & 0x0f) << 4) | ((c3 & 0x3c) >> 2);
+		if (bp >= buffer + len)
+			break;
 
-                p += 4;
-        }
-		return (bp == buffer + len);
+		c4 = CHAR64(*(p + 3));
+		if (c4 == 255)
+			break;
+
+		*bp++ = ((c3 & 0x03) << 6) | c4;
+
+		p += 4;
+	}
+	if (bp < buffer + len)
+		printf("len: %d filled: %d\n", len,
+			   (int)(((uintptr_t)bp) - ((uintptr_t)buffer)));
+
+	return (bp >= buffer + len);
 }
 
 static void
 encode_base64(u_int8_t *buffer, u_int8_t *data, u_int16_t len)
 {
-        u_int8_t *bp = buffer;
-        u_int8_t *p = data;
-        u_int8_t c1, c2;
-        while (p < data + len) {
-                c1 = *p++;
-                *bp++ = Base64Code[(c1 >> 2)];
-                c1 = (c1 & 0x03) << 4;
-                if (p >= data + len) {
-                        *bp++ = Base64Code[c1];
-                        break;
-                }
-                c2 = *p++;
-                c1 |= (c2 >> 4) & 0x0f;
-                *bp++ = Base64Code[c1];
-                c1 = (c2 & 0x0f) << 2;
-                if (p >= data + len) {
-                        *bp++ = Base64Code[c1];
-                        break;
-                }
-                c2 = *p++;
-                c1 |= (c2 >> 6) & 0x03;
-                *bp++ = Base64Code[c1];
-                *bp++ = Base64Code[c2 & 0x3f];
-        }
-        *bp = '\0';
+	u_int8_t *bp = buffer;
+	u_int8_t *p = data;
+	u_int8_t c1, c2;
+	while (p < data + len) {
+		c1 = *p++;
+		*bp++ = Base64Code[(c1 >> 2)];
+		c1 = (c1 & 0x03) << 4;
+		if (p >= data + len) {
+			*bp++ = Base64Code[c1];
+			break;
+		}
+		c2 = *p++;
+		c1 |= (c2 >> 4) & 0x0f;
+		*bp++ = Base64Code[c1];
+		c1 = (c2 & 0x0f) << 2;
+		if (p >= data + len) {
+			*bp++ = Base64Code[c1];
+			break;
+		}
+		c2 = *p++;
+		c1 |= (c2 >> 6) & 0x03;
+		*bp++ = Base64Code[c1];
+		*bp++ = Base64Code[c2 & 0x3f];
+	}
+	*bp = '\0';
 }
 
 static bool
 key_from_base64(uint8_t key[static WG_KEY_LEN], const char *base64)
 {
 
-	if (strlen(base64) != WG_KEY_LEN_BASE64 - 1 || base64[WG_KEY_LEN_BASE64 - 2] != '=')
+	if (strlen(base64) != WG_KEY_LEN_BASE64 - 1) {
+		warnx("bad key len - need %d got %lu\n", WG_KEY_LEN_BASE64 - 1, strlen(base64));
 		return false;
-
+	}
+	if (base64[WG_KEY_LEN_BASE64 - 2] != '=') {
+		warnx("bad key terminator, expected '=' got '%c'", base64[WG_KEY_LEN_BASE64 - 2]);
+		return false;
+	}
 	return (decode_base64(key, WG_KEY_LEN, base64));
 }
 
